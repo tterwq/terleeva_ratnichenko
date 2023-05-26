92d81727a834fa33bb4b90c0987040e057d67d41
AnastasiaRatko <anastasiarathichenko@gmail.com>
2023-05-27 01:41:45 +0600

    тесты

---
+++ SpaceBattle.Lib.Test/GameClassTest/GameObjectsTests.cs
@@ -0,0 +1,96 @@
+using Hwdtech;
+using Hwdtech.Ioc;
+using Moq;
+
+
+namespace SpaceBattle.Lib.Test;
+
+public class GameObjectsTests
+{
+    public GameObjectsTests()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+    }
+
+    [Fact]
+    public void getItemTest()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SpaceBattle.Operation.Game.CreateNew", (object[] args) => new ActionCommand(
+            () =>
+            {
+                IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", args[0]).Execute();
+            }
+        )).Execute();
+
+        ICommand gameCommand = (ICommand)new CreateNewGame().Strategy();
+        gameCommand.Execute();
+
+        var mockObj = new Mock<IUObject>();
+
+        IoC.Resolve<Dictionary<string, IUObject>>("General.Objects").Add("0", mockObj.Object);
+
+        var resolvedObj = IoC.Resolve<IUObject>("General.GetItem", "0");
+
+        Assert.Equal(mockObj.Object, resolvedObj);
+    }
+
+    [Fact]
+    public void removeItemTest()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SpaceBattle.Operation.Game.CreateNew", (object[] args) => new ActionCommand(
+            () =>
+            {
+                IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", args[0]).Execute();
+            }
+        )).Execute();
+
+        ICommand gameCommand = (ICommand)new CreateNewGame().Strategy();
+        gameCommand.Execute();
+
+        var mockObj = new Mock<IUObject>();
+
+        IoC.Resolve<Dictionary<string, IUObject>>("General.Objects").Add("0", mockObj.Object);
+        Assert.True(IoC.Resolve<Dictionary<string, IUObject>>("General.Objects").Count() == 1);
+
+        IoC.Resolve<ICommand>("General.RemoveItem", "0").Execute();
+        Assert.True(IoC.Resolve<Dictionary<string, IUObject>>("General.Objects").Count() == 0);
+    }
+    
+    [Fact]
+    public void getItemTestObjectNotExists()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SpaceBattle.Operation.Game.CreateNew", (object[] args) => new ActionCommand(
+            () =>
+            {
+                IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", args[0]).Execute();
+            }
+        )).Execute();
+
+        ICommand gameCommand = (ICommand)new CreateNewGame().Strategy();
+        gameCommand.Execute();
+
+        var mockObj = new Mock<IUObject>();
+
+        IoC.Resolve<Dictionary<string, IUObject>>("General.Objects").Add("0", mockObj.Object);
+
+        IoC.Resolve<ICommand>("General.RemoveItem", "0").Execute();
+        Assert.True(IoC.Resolve<Dictionary<string, IUObject>>("General.Objects").Count() == 0);
+
+        Assert.Throws<Exception>(
+            () =>
+            {
+                IoC.Resolve<IUObject>("General.GetItem", "0");
+            }
+        );
+    }
+
+}
---
+++ SpaceBattle.Lib.Test/GameClassTest/QueueStrategiesTests.cs
@@ -0,0 +1,70 @@
+using Hwdtech;
+using Hwdtech.Ioc;
+using Moq;
+
+
+namespace SpaceBattle.Lib.Test;
+
+public class QueueStrategiesTests
+{
+    public QueueStrategiesTests()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SpaceBattle.Operation.Game.CreateNew", (object[] args) => new ActionCommand(
+            () =>
+            {
+                IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", args[0]).Execute();
+            }
+        )).Execute();
+    }
+
+    [Fact]
+    public void enqueueTest()
+    {
+        var queue = new Queue<ICommand>();
+        var cmd = new Mock<ICommand>();
+
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SpaceBattle.Operation.Game.CreateNew", (object[] args) => new ActionCommand(
+            () =>
+            {
+                IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", args[0]).Execute();
+            }
+        )).Execute();
+
+        ICommand gameCommand = (ICommand)new CreateNewGame().Strategy();
+        gameCommand.Execute();
+
+        IoC.Resolve<ICommand>("GetOutGueue", queue, cmd.Object).Execute();
+        Assert.True(queue.Count() == 1);
+    }
+    
+    [Fact]
+    public void dequeueTest()
+    {
+        var queue = new Queue<ICommand>();
+        var cmd = new Mock<ICommand>();
+
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SpaceBattle.Operation.Game.CreateNew", (object[] args) => new ActionCommand(
+            () =>
+            {
+                IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", args[0]).Execute();
+            }
+        )).Execute();
+
+        ICommand gameCommand = (ICommand)new CreateNewGame().Strategy();
+        gameCommand.Execute();
+
+        IoC.Resolve<ICommand>("GetOutGueue", queue, cmd.Object).Execute();
+
+        var cmd1 = IoC.Resolve<ICommand>("GetOutGueue", queue);
+        Assert.Equal(cmd.Object, cmd1);
+    }
+}
