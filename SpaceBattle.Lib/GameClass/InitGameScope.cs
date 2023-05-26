04bdf1f5ed522eca019e071b6f77617f14e8efa7
AnastasiaRatko <anastasiarathichenko@gmail.com>
2023-05-27 00:18:43 +0600

    удаление из очереди

--- SpaceBattle.Lib/GameClass/InitGameScope.cs
+++ SpaceBattle.Lib/GameClass/InitGameScope.cs
@@ -11,18 +11,18 @@ public class InitGameScope : IStrategy
         object scope = IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"));
         IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", scope).Execute();
 
+        Dictionary<string, IUObject> gameObjects = new Dictionary<string, IUObject>();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "General.Objects", (Func<object[], Dictionary<string, IUObject>>)(args => gameObjects)).Execute();
+        
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Scopes.Outer", (Func<object[], object>)(args => scopePrev)).Execute();
 
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "GetQuantum", (Func<object[], object>)(args => (int)_args[0])).Execute();
 
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "General.GetItem", (Func<object[], IUObject>)(args => (IUObject)new GetItem().RunStrategy(args[0]))).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "General.GetItem", (Func<object[], IUObject>)(args => (IUObject)new GetItem().Strategy(args[0]))).Execute();
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "General.RemoveItem", (Func<object[], ICommand>)(args => new RemoveItemCommand((string)args[0]))).Execute();
  
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "QueueDequeue", (Func<object[], ICommand>)(args => (ICommand)new QueueDequeue().Strategy((Queue<ICommand>)args[0]))).Execute();
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "QueueEnqueue", (Func<object[], ICommand>)(args => new QueueEnqueueCommand((Queue<ICommand>)args[0], (ICommand)args[1]))).Execute();
-
-        Dictionary<string, IUObject> gameObjects = new Dictionary<string, IUObject>();
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "General.Objects", (Func<object[], Dictionary<string, IUObject>>)(args => gameObjects)).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "GetOutQueue", (Func<object[], ICommand>)(args => (ICommand)new GetOutQueue().Strategy((Queue<ICommand>)args[0]))).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "PutInQueue", (Func<object[], ICommand>)(args => new PutInQueue((Queue<ICommand>)args[0], (ICommand)args[1]))).Execute();
 
         IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.Outer")).Execute();
 
