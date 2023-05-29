f416bc495c904ea4988b20ad388cf71f1b376aa0
tterwq <tterleeva28@gmail.com>
2023-05-29 18:36:28 +0600

    8laba

--- SpaceBattle.Lib.Test/EndMoveCommand/EndMoveCommandTest.cs
+++ SpaceBattle.Lib.Test/EndMoveCommand/EndMoveCommandTest.cs
@@ -3,9 +3,9 @@ using Hwdtech.Ioc;
 
 namespace SpaceBattle.Lib.Test;
 
-public class EndMoveCommandeTest
+public class EndMoveCommandTest
 {
-    public EndMoveCommandeTest()
+    public EndMoveCommandTest()
     {
         new InitScopeBasedIoCImplementationCommand().Execute();
         IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
---
+++ SpaceBattle.Lib/Using.cs
@@ -0,0 +1,6 @@
+global using System;
+global using System.Collections.Concurrent;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using Hwdtech;
+global using System.Diagnostics;
