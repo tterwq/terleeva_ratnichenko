c09f48c50c2d19c89c954fc1fab71d80e90e9dba
tterwq <tterleeva28@gmail.com>
2023-10-05 01:17:34 +0600

    api2

--- SpaceBattle.Lib/Message/IMessage.cs
+++
@@ -1,9 +0,0 @@
-namespace SpaceBattle.Lib;
-
-public interface IMessage
-{
-    public string type {get;}
-    public string gameId {get;}
-    public string gameItemId {get;}
-    public IDictionary<string, object> properties {get;}
-}
