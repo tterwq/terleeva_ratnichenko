29d8502aa616d52987787009e5402f6cc5e33940
tterwq <tterleeva28@gmail.com>
2023-10-05 01:20:41 +0600

    api3  no tests

---
+++ SpaceBattle.Lib/API/WebAPI.cs
@@ -0,0 +1,22 @@
+﻿using CoreWCF;
+using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+[ServiceBehavior(IncludeExceptionDetailInFaults = true)]
+internal class WebApi : IWebApi
+{
+    public void GetOperationMessage(MessageContract mc)
+    {
+        try
+        {
+            var threadId = IoC.Resolve<Dictionary<string, object>>("GetThreadID", mc);
+            IoC.Resolve<ICommand>("SendCommand", threadId, IoC.Resolve<ICommand>("CreateCommandFromMessage", mc)).Execute();
+
+        }
+        catch (Exception e)
+        {
+            throw IoC.Resolve<Exception>("SystemException", e);
+        }
+    }
+}
